79897ae275220e2cb76d1e0a9c16e328564fc139
matt <ethacktry@gmail.com>
2023-04-21 18:58:36 +0200

    Changed logClick to a switch statement, covering a range of input conditions

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
 const display = document.querySelector("#display");
 const eventField = document.querySelector("#button-container");
 eventField.addEventListener("click", logClick);
+display.innerText = "0";
+
+const dec = document.querySelector(".decimal");
+let decimalEntered = false;
+let a, b, operator;
 
 function logClick(e) {
+	const button = e.target;
 	if (e.target.classList.contains("button")) {
-		if (e.target.classList.contains("clear")) {
-			display.innerText = "0";
-		} else if (e.target.classList.contains("delete")) {
-			display.innerText = display.innerText.slice(0, -1);
-			if (display.innerText.length === 0) {
+		switch (true) {
+			case button.classList.contains("clear"):
 				display.innerText = "0";
-			}
-		} else if (display.innerText === "0" && e.target.value !== "0") {
-			display.innerText = e.target.innerText;
-		} else {
-			display.innerText += e.target.innerText;
+				decimalEntered = false;
+				break;
+			case button.classList.contains("delete"):
+				if (display.innerText.charAt(display.innerText.length - 1) === ".") {
+					display.innerText = display.innerText.slice(0, -1) || "0";
+					decimalEntered = false;
+				} else {
+					display.innerText = display.innerText.slice(0, -1) || "0";
+				}
+				break;
+			case button.innerText === ".":
+				if (!decimalEntered) {
+					display.innerText += button.innerText;
+					decimalEntered = true;
+				}
+				break;
+			// case button.value === "." && display.innerText === "0":
+			// 	display.innerText = "0.";
+			// 	break;
+			case display.innerText === "0" && button.value !== "0":
+				display.innerText = button.innerText;
+				break;
+			default:
+				display.innerText += button.innerText;
 		}
-	}
+	} else return;
 }
 
-let a, b, operator;
+function operate(a, b, operator) {
+	switch (operator) {
+		case "+":
+			return add(a, b);
+		case "-":
+			return subtract(a, b);
+		case "*":
+			return multiply(a, b);
+		case "/":
+			return divide(a, b);
+	}
+}
 
 function add(a, b) {
 	return a + b;
@@ -34,17 +67,4 @@ function multiply(a, b) {
 }
 function divide(a, b) {
 	return a / b;
-}
-
-function operate(a, b, operator) {
-	switch (operator) {
-		case "+":
-			return add(a, b);
-		case "-":
-			return subtract(a, b);
-		case "*":
-			return multiply(a, b);
-		case "/":
-			return divide(a, b);
-	}
 }
