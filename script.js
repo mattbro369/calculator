9da3e9125a89f4d71033d96892193b981dd789c0
matt <ethacktry@gmail.com>
2023-05-05 10:50:52 +0200

    Added keyboard support. Need to fully test

--- script.js
+++ script.js
@@ -1,81 +1,165 @@
+//TODO There is an error which fires for button.classList
+//TODO Fully check button functionality and click func works
+//TODO CSS TIME BABY
+
 const display = document.querySelector("#display");
 const displayInitial = document.querySelector("#initial");
 const eventField = document.querySelector("#button-container");
 const displaySecond = document.createElement("div");
 eventField.addEventListener("click", logClick);
+window.addEventListener("keydown", (event) => {
+	logPress(event);
+	logClick(event, button);
+});
 displayInitial.innerText = "0";
 
 let decimalEntered = false;
 let a, b, operator, answer;
 let operatorArr = [];
+let button;
 
-function logClick(e) {
-	const button = e.target;
+function logPress(event) {
+	let key = event.code;
+	console.log(key);
 
-	if (e.target.classList.contains("button")) {
-		switch (true) {
-			case button.classList.contains("clear"):
-				clearDisplay();
-				break;
-			case button.classList.contains("delete"):
-				if (displayInitial.innerText.charAt(0) === "=") return;
-				if (displayInitial.innerText !== "0") removeLastChar();
-				if (displayInitial.innerText.length < 1) {
-					displayInitial.innerText = "0";
-				}
-				break;
-			case button.innerText === ".":
-				if (displayInitial.innerText.charAt(0) === "=") return;
-				if (!decimalEntered) {
-					displayInitial.innerText += button.innerText;
-					decimalEntered = true;
-				}
-				break;
-			case button.classList.contains("operator"):
-				if (operatorArr.length === 0) {
-					getNumber();
-					getOperator(button);
-					displayInitial.innerText += button.innerText;
-					addInnerDisplay();
-					displaySecond.innerText = `${a} ${operator}`;
-					displayInitial.innerText = "0";
-					decimalEntered = false;
-				} else if (operatorArr.length === 1) {
-					getOperator(button);
-					getNumber();
-					operate(operatorArr);
-					displaySecond.innerText = `${a} ${operatorArr[0]}`;
-					displayInitial.innerText = "0";
-					decimalEntered = false;
-				}
+	switch (key) {
+		case "Numpad0":
+		case "Digit0":
+			key = document.getElementById("0");
+			break;
+		case "Numpad1":
+		case "Digit1":
+			key = document.getElementById("1");
+			break;
+		case "Numpad2":
+		case "Digit2":
+			key = document.getElementById("2");
+			break;
+		case "Numpad3":
+		case "Digit3":
+			key = document.getElementById("3");
+			break;
+		case "Numpad4":
+		case "Digit4":
+			key = document.getElementById("4");
+			break;
+		case "Numpad5":
+		case "Digit5":
+			key = document.getElementById("5");
+			break;
+		case "Numpad6":
+		case "Digit6":
+			key = document.getElementById("6");
+			break;
+		case "Numpad7":
+		case "Digit7":
+			key = document.getElementById("7");
+			break;
+		case "Numpad8":
+		case "Digit8":
+			key = document.getElementById("8");
+			break;
+		case "Numpad9":
+		case "Digit9":
+			key = document.getElementById("9");
+			break;
+		case "NumpadMultiply":
+			key = document.getElementById("multiply");
+			break;
+		case "NumpadAdd":
+			key = document.getElementById("add");
+			break;
+		case "NumpadSubtract":
+			key = document.getElementById("subtract");
+			break;
+		case "NumpadDivide":
+			key = document.getElementById("divide");
+			break;
+		case "NumpadDecimal":
+			key = document.getElementById("decimal");
+			break;
+		case "NumpadEnter":
+		case "Enter":
+			key = document.getElementById("equals");
+			break;
+		case "Backspace":
+		case "Delete":
+			key = document.getElementById("delete");
+			break;
+		default:
+			break;
+	}
+	button = key;
+	return button;
+}
 
-				break;
-			case displayInitial.innerText === "0" && button.value !== "0":
-				displayInitial.innerText = button.innerText;
-				break;
-			case button.classList.contains("equals"):
-				if (
-					displayInitial.innerText === "0" ||
-					displayInitial.innerText.charAt(0) === "=" ||
-					operatorArr.length === 0
-				)
-					return;
+function logClick(e, button) {
+	if (!logPress(e) && e.target.classList.contains("button")) {
+		button = e.target;
+	}
+
+	switch (true) {
+		case button.id === "clear":
+			clearDisplay();
+			break;
+		case button.id === "delete":
+			if (displayInitial.innerText.charAt(0) === "=") return;
+			if (displayInitial.innerText !== "0") removeLastChar();
+			if (displayInitial.innerText.length < 1) {
+				displayInitial.innerText = "0";
+			}
+			break;
+		case button.id === "decimal":
+			if (displayInitial.innerText.charAt(0) === "=") return;
+			if (!decimalEntered) {
+				displayInitial.innerText += button.innerText;
+				decimalEntered = true;
+			}
+			break;
+		case button.classList.contains("operator"):
+			if (operatorArr.length === 0) {
 				getNumber();
-				operate(operator);
-				displaySecond.innerText += " " + displayInitial.innerText;
-				displayInitial.innerText = "=" + a;
+				getOperator(button);
+				displayInitial.innerText += button.innerText;
+				addInnerDisplay();
+				displaySecond.innerText = `${a} ${operator}`;
+				displayInitial.innerText = "0";
 				decimalEntered = false;
-				operatorArr = [];
-				break;
-			default:
-				if (displayInitial.innerText.charAt(0) === "=") {
-					clearDisplay();
-					displayInitial.innerText = button.innerText;
-				} else {
-					displayInitial.innerText += button.innerText;
-				}
-		}
-	} else return;
+			} else if (operatorArr.length === 1) {
+				getOperator(button);
+				getNumber();
+				operate(operatorArr);
+				displaySecond.innerText = `${a} ${operatorArr[0]}`;
+				displayInitial.innerText = "0";
+				decimalEntered = false;
+			}
+
+			break;
+		case displayInitial.innerText === "0" && button.value !== "0":
+			displayInitial.innerText = button.innerText;
+			break;
+		case button.id === "equals":
+			if (
+				displayInitial.innerText === "0" ||
+				displayInitial.innerText.charAt(0) === "=" ||
+				operatorArr.length === 0
+			)
+				return;
+			getNumber();
+			operate(operator);
+			displaySecond.innerText += " " + displayInitial.innerText;
+			displayInitial.innerText = "=" + a;
+			decimalEntered = false;
+			operatorArr = [];
+			break;
+		default:
+			if (displayInitial.innerText.charAt(0) === "=") {
+				clearDisplay();
+				displayInitial.innerText = button.innerText;
+			} else {
+				displayInitial.innerText += button.innerText;
+			}
+	}
 }
 
 function operate(operatorArr) {
